8a880e521a58756d69ca17ea354169ac33eee4c9
1aerostorm <aerostorm1@gmail.com>
2023-05-29 17:34:22 +0000

    Fix showToast for ConvertToSteem

--- app/components/elements/Notifications/ToastUtils.jsx
+++ app/components/elements/Notifications/ToastUtils.jsx
@@ -3,29 +3,31 @@ import { toast } from 'react-hot-toast'
 import tt from 'counterpart'
 
 export function showToast(content, opts = {}) {
-    toast((t) => <React.Fragment><div className='toast-content'>
-                {content}
-            </div>
-            <div className='toast-action' onClick={e => {
-                toast.dismiss(t.id)
-            }}>
-                {opts.action || tt('g.dismiss')}
-            </div>
-        </React.Fragment>, {
-        duration: opts.dismissAfter || 5000,
-        style: {
-            padding: '0px',
-            'width': '100%'
-        },
-        ariaProps: {
+    setTimeout(() => {
+        toast((t) => <React.Fragment><div className='toast-content'>
+                    {content}
+                </div>
+                <div className='toast-action' onClick={e => {
+                    toast.dismiss(t.id)
+                }}>
+                    {opts.action || tt('g.dismiss')}
+                </div>
+            </React.Fragment>, {
+            duration: opts.dismissAfter || 5000,
             style: {
-                'margin': '0px',
-                'border-radius': '8px',
-                'max-width': '28rem',
+                padding: '0px',
                 'width': '100%'
+            },
+            ariaProps: {
+                style: {
+                    'margin': '0px',
+                    'border-radius': '8px',
+                    'max-width': '28rem',
+                    'width': '100%'
+                }
             }
-        }
-    })
+        })
+    }, 1)
 }
 
 export function showCustomToast(content, opts = {}) {
