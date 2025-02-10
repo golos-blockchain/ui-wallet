733000b00a4087762f8a7bc0d3a9319ec9828f22
1aerostorm <aerostorm1@gmail.com>
2025-02-10 23:04:14 +0300

    Mobile app - Show version

--- app/app_cfg.js
+++ app/app_cfg.js
@@ -1,2 +1,58 @@
-/* Only Mobile. Generated automatically. Do not edit or remove from GitHub. */
-module.exports = {}
+/* Only Mobile. Generated automatically. Do not edit. */
+module.exports = {
+    "app_version": "1.0.0",
+    "ws_connection_app": [
+        {
+            "address": "wss://apibeta.golos.today/ws"
+        },
+        {
+            "address": "wss://api.golos.id/ws"
+        },
+        {
+            "address": "wss://api.aleksw.space/ws"
+        },
+        {
+            "address": "wss://api-golos.blckchnd.com/ws"
+        }
+    ],
+    "ws_connection_exchange": "wss://apibeta.golos.today/ws",
+    "logo": {
+        "icon": "https://i.imgur.com/mRV2Je1.png",
+        "title": "https://i.imgur.com/CYfDqIy.png"
+    },
+    "images": {
+        "img_proxy_prefix": "https://devimages.golos.today",
+        "img_proxy_backup_prefix": "https://steemitimages.com",
+        "upload_image": "https://api.imgur.com/3/image",
+        "client_id": "6c09ebf8c548126"
+    },
+    "blogs_service": {
+        "host": "https://beta.golos.today"
+    },
+    "messenger_service": {
+        "host": "https://devchat.golos.app"
+    },
+    "auth_service": {
+        "host": "https://dev.golos.app",
+        "custom_client": "blogs"
+    },
+    "notify_service": {
+        "host": "https://devnotify.golos.app",
+        "host_ws": "wss://devnotify.golos.app/ws"
+    },
+    "apidex_service": {
+        "host": "https://devapi-dex.golos.app",
+        "host_local": "https://devapi-dex.golos.app"
+    },
+    "app_updater": {
+        "host": "https://devfiles.golos.app"
+    },
+    "hidden_assets": {
+        "RUDEX": true,
+        "PRIZM": true,
+        "DOGECOIN": true,
+        "YMZEC": true,
+        "YMWMZ": true,
+        "YMBTC": true
+    }
+}
