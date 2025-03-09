a15456fdf3fd70edbb4f864faed092b91b8928c0
1aerostorm <aerostorm1@gmail.com>
2025-03-09 07:03:26 +0300

    Smart Exchange - optimize performance

--- shared/getExchangeData.js
+++ shared/getExchangeData.js
@@ -1,4 +1,4 @@
-import { Asset } from 'golos-lib-js/lib/utils'
+import { Asset, Price } from 'golos-lib-js/lib/utils'
 import { api } from 'golos-lib-js'
 
 export const MIN_PROFIT_PCT = 5
@@ -14,22 +14,31 @@ export const ExchangeTypes = {
     }
 }
 
+// works like apidexExchange result
+const wrapDir = (resMul) => { 
+    const { direct } = resMul
+    const { steps, best_price, limit_price } = direct
+    if (!steps) {
+        return { error: 'no_orders' }
+    }
+
+    const res = {}
+    res.result = Asset(direct.res)
+    res.limit_price = Price(Asset(limit_price.quote), Asset(limit_price.base))
+    res.best_price = Price(Asset(best_price.quote), Asset(best_price.base))
+    if (steps[0].remain) {
+        res.remain = Asset(steps[0].remain)
+    }
+
+    return res
+}
+
 export default async function getExchangeData(endpoint,
 	amount, symbol, direction = 'sell', eType = ExchangeTypes.direct,
 	min_to_receive = null) {
 	const { dex, exNode, callParams } = endpoint
 	try {
         let resDir
-        resDir = await dex.apidexExchange({
-            sell: amount,
-            buySym: symbol,
-            direction,
-        })
-
-        let resCurr
-        if (resDir && resDir.result && eType == ExchangeTypes.direct) {
-            resCurr = resDir.result.clone()
-        }
 
         let mtr = {}
         if (min_to_receive) {
@@ -37,40 +46,49 @@ export default async function getExchangeData(endpoint,
                 multi: min_to_receive
             }
         } else {
-            if (direction === 'sell') {
-                if (resCurr) {
-                    const multi = resCurr.clone()
-                    let more = multi.mul(100 + MIN_PROFIT_PCT).div(100)
-                    if (more.eq(multi)) more = more.plus(1)
-                    mtr.multi = more
-                }
-            } else {
+            if (direction === 'buy') {
                 const multi = Asset(amount)
                 let lesser = multi.mul(100 - MIN_PROFIT_PCT).div(100)
                 if (lesser.eq(0)) lesser.amount = 1
                 mtr.multi = lesser
+            } else if (eType === ExchangeTypes.direct) {
+                mtr.min_profit_pct = MIN_PROFIT_PCT
             }
         }
 
         let resMul, errMul
-        try {
-            resMul = await dex.getExchange({
-                node: exNode ? exNode() : undefined,
+        if (Asset(amount).gt(0)) {
+            try {
+                resMul = await dex.getExchange({
+                    node: exNode ? exNode() : undefined,
 
-                amount,
-                direction,
-                symbol,
-                hybrid: {
-                    strategy: 'discrete',
-                },
-                remain: {
-                    multi: 'ignore'
-                },
-                min_to_receive: mtr
-            })
-        } catch (err) {
-            console.error('Multi-step getExchange error:', err)
-            errMul = err.toString()
+                    amount,
+                    direction,
+                    symbol,
+                    hybrid: {
+                        strategy: 'spread',
+                    },
+                    remain: {
+                        multi: 'ignore'
+                    },
+                    min_to_receive: mtr
+                })
+            } catch (err) {
+                console.error('Multi-step getExchange error:', err)
+                errMul = err.toString()
+            }
+
+            try {
+                resDir = wrapDir(resMul)
+            } catch (err) {
+                console.error('Cannot obtain direct chain from getExchange response', err)
+                resDir = await dex.apidexExchange({
+                    sell: amount,
+                    buySym: symbol,
+                    direction,
+                })
+                console.log('...correct res is', resDir)
+            }
         }
 
         return {
