0f3cac639d77f8ed6fc8f81228862b42f05d28ce
Lex-Ai <12001684+Lex-Ai@users.noreply.github.com>
2025-04-16 23:51:11 +0300

    Update quickBuy.js

--- app/quickBuy.js
+++ app/quickBuy.js
@@ -5,22 +5,18 @@
 
 export default {
     "YMUSDT": {
-        "text": "Вы можете купить токены USDT TRC-20 (Tether в сети Tron) в обменниках <extlink href=\"https://onemoment.cc/?cur_from=SBERRUB&cur_to=USDTTRC20\">OneMoment</extlink> или <extlink href=\"https://sova.gg/?cur_from=SBERRUB&cur_to=USDTTRC20\">Sova</extlink>.<br/>Либо выбрать любой иной способ из рейтинга на сайте <extlink href=\"https://www.bestchange.ru/sberbank-to-tether-trc20.html\">BestChange</extlink>."
+        "text": "Вы можете купить токены USDT TON (Tether в сети TON) в обменниках из рейтинга <extlink href=\"https://www.bestchange.ru/sberbank-to-tether-ton.html\">BestChange</extlink>."
     },
 
-    "YMRUB": {
-        "text": "Вы можете сделать перевод банковской картой <extlink href=\"https://qiwi.com/n/GEDUA209\">с сайта QIWI</extlink> (в поле Комментарий обязательно укажите golos:аккаунт)."
+    "YMTON": {
+        "text": "Вы можете купить токены TON в обменниках из рейтинга <extlink href=\"https://www.bestchange.ru/sberbank-to-ton.html\">BestChange</extlink>."
     },
- 
+
     "YMPZM": {
         "text": "Вы можете купить токены PZM на бирже <extlink href=\"https://market.rudex.org/#/market/RUDEX.PZM_RUDEX.USDT\">RuDEX</extlink> или совершить перевод напрямую с <extlink href=\"https://wallet.prizm.vip\">кошелька PRIZM</extlink>."
     },
  
-    "YMHIVE": {
-        "text": "Вы можете купить токены HIVE на бирже <extlink href=\"https://www.binance.com/en/trade/HIVE_USDT\">Binance</extlink> или совершить перевод напрямую с <extlink href=\"https://wallet.hive.blog\">кошелька HIVE</extlink>."
-    },
- 
-    "YMSTEEM": {
-        "text": "Вы можете купить токены STEEM на бирже <extlink href=\"https://www.binance.com/en/trade/STEEM_USDT\">Binance</extlink> или совершить перевод напрямую с <extlink href=\"https://steemitwallet.com\">кошелька STEEM</extlink>."
+    "YMXMR": {
+        "text": "Вы можете купить токены XMR в обменниках из рейтинга <extlink href=\"https://www.bestchange.ru/sberbank-to-monero.html\">BestChange</extlink>."
     }
 }
